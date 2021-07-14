b84f63802b3a160a7e6096c2eb6508fcc27c2133
Eddie Yeung <eycs0317@gmail.com>
2021-07-13 18:28:21 -0700

    able to prompt user and get data back

--- index.js
+++ index.js
@@ -1,9 +1,59 @@
-var inquirer = require('inquirer');
-
 // TODO: Include packages needed for this application
-
+var inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+  type: 'input',
+  name: 'username',
+  message: `What is your GitHub username?`
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: `What is your email address?`
+  },
+  {
+    type: 'input',
+    name: 'projectName',
+    message: `What is your project's name`
+  },
+  {
+    type: 'input',
+     name: 'description',
+     message: `Please write a short description of your project?`
+  },
+  {
+    type: 'input',
+     name: 'license',
+     message: `What kind of license should your project have?`
+  },
+  {
+    type: 'input',
+     name: 'dependencies',
+     message: `What command should be run to install dependencies?`
+  },
+  {
+    type: 'input',
+     name: 'testCommand',
+     message: `What command should be run to run tests?`
+  },
+  {
+    type: 'input',
+     name: 'repoUse',
+     message: `What does the user need to know about using the repo?`
+  },
+  {
+    type: 'input',
+     name: 'test',
+     message: `What command should be run to run tests?`
+  },
+  {
+    type: 'input',
+     name: 'contributing',
+     message: `What does the user need to know about contributing to the repo?`
+  }
+];
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -13,3 +63,7 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+inquirer.prompt(questions).then(answers => {
+  console.log(answers)
+})
